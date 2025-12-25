411b2dfac4096537adb6d60b9c50c1db04743e49
valerij <valerijkolpacov@gmail.com>
2025-12-25 21:44:55 +0200

    add task solution Calendar

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,9 +3,5 @@ module.exports = {
   plugins: [
     "stylelint-scss"
   ],
-  rules: {
-    "at-rule-empty-line-before": null,
-    "declaration-empty-line-before": null,
-    "order/properties-order": null
-  }
+
 };
